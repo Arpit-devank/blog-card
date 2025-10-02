2ea8367c050034f90d791c5003d32c3c4fc8c0ce
Arpit <arpit200960632242@gmail.com>
2025-10-02 23:38:17 +0530

    bug fix

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ document.querySelector("#app").innerHTML = `
   <div class="min-h-screen bg-Yellow flex flex-col items-center justify-center font-figtree">
 
     <div class="bg-White border w-[290px] p-4 rounded-xl shadow-3xl">
-    <img src="/assets/images/illustration-article.svg" class="rounded-md">
+    <img src="assets/images/illustration-article.svg" class="rounded-md">
 
     <h1 class="bg-Yellow px-4 py-1 my-3 rounded-md inline-block font-bold text-sm">Learning</h1>
     <h2 class="font-medium text-sm">Published 21 Dec 2023</h2>
@@ -14,7 +14,7 @@ document.querySelector("#app").innerHTML = `
     These languages are the backbone of every website, defining structure, content, and presentation.
     </p>
     <div class="flex gap-4 my-4 items-center">
-    <img class="size-8" src="/assets/images/image-avatar.webp">
+    <img class="size-8" src="assets/images/image-avatar.webp">
     <div class="font-bold">Greg Hooper</div>
     </div>
     </div>
